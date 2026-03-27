e4c165522292056b18980f3cc66bcbbe8f35881d
jrnibs <johnnibato13@gmail.com>
2026-03-27 19:39:26 +0800

    lesson and calculator

--- OOP/class.php
+++ OOP/class.php
@@ -1,9 +1,29 @@
 <?php
 
-class BankAccount {
+class Account{
     private $accountNumber; // properties
+
+    public function __construct($accountNumber) {  
+        $this->accountNumber = $accountNumber;
+    }
+
+    public function getAccountNumber(){
+        return $this->accountNumber;
+    }
+
+    public function setAccountNumber($accountNumber){
+        $this->accountNumber = $accountNumber;
+    }
+}
+
+class BankAccount {
+
     private $balance; // properties
     
+    public function __construct($balance){
+        $this->balance = $balance;
+    }
+
     private function deposit($amount) {
         if($amount > 0){
             $this->balance += $amount;
@@ -27,23 +47,106 @@ class BankAccount {
         return false;
     }
 
-    public function getAccountNumber(){
-        return $this->accountNumber;
-    }
     public function getbalance(){
         return $this->balance;
     }
 
-    public function setAccountNumber($accountNumber){
-        $this->accountNumber = $accountNumber;
+}
+
+class SavingsAccount extends BankAccount{
+    private $interestRate;
+
+    public function __construct($balance, $interestRate){
+        parent::__construct($balance);
+        $this->interestRate = $interestRate;
+    }
+
+    public function getInterestRate(){
+        return $this->interestRate;
+    }
+
+    public function setInterestRate($interestRate){
+        $this->interestRate = $interestRate;
+    }
+
+    public function addInterest(){
+        if($this->getbalance() > 0){
+            $interest = ($this->getbalance() * ($this->interestRate /100)) / 365;
+            $this->transaction($interest, "deposit");
+            return true;
+        }
+        return false;
+    }
+
+}
+
+class CheckingAccount extends BankAccount{
+    private $minBalance;
+
+    public function __construct($balance, $minBalance){
+        parent::__construct($balance);
+        $this->minBalance = $minBalance;
+    }
+
+    public function withdraw($amount){
+        if(($this->getbalance() - $this->minBalance) >= $amount){
+            $this->transaction($amount, "withdraw");
+            return true;
+        }
+        return false;
+    }
+
+    public function getminBalance()
+    {
+        return $this->minBalance;
     }
 }
 
 class Customer {
-    public $name;
+    private $name;
+    private $email;
+    private $phone;
+    private $address;
+    private $accounts = [];
+
+    public function __construct($name,$email,$phone,$address)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->address= $address;
+        $this->accounts = [];
+    }
+
+    public function openAccount($account, $bankAccount){
+        $this->accounts[] = ['account'=>$account,'bankaccount'=>$bankAccount];
+    }
+
+    public function closeAccount($accountToClose){
+        foreach($this->accounts as $index => $accountData){
+            if($accountData['account']->getAccountNumber() === $accountToClose->getAccountNumber()){
+                unset($this->accounts[$index]);
+                return true;
+            }
+        }
+        return false;
+    }
 
     public function getName(){
         return $this->name;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    public function getPhone(){
+        return $this->phone;
+    }
+    public function getAddress(){
+        return $this->address;
+    }
+
+    public function getAccounts(){
+        return $this->accounts ?? [];
     }
 
     public function setName($name){
@@ -55,5 +158,39 @@ class Customer {
         }
         $this->name = $name;
         return true;
+    }
+}
+
+class Bank{
+    private $customers;
+
+    public function __construct(){
+        $this->customers = [];
+    }
+
+    public function getCustomer(){
+        return $this->customers;
+    }
+
+    public function addCustomer($customer){
+        $this->customers[] = $customer;
+    }
+
+    public function removeCustomer($customer){
+        $index = array_search($customer, $this->customers);
+
+        if($index != false){
+            unset($this->customers[$index]);
+            return true;
+        }
+        return false;
+    }
+
+    public function processTransaction($account, $amount){
+        if($amount > 0){
+            $account->transaction($amount, "deposit");
+        } else {
+            $account->transaction(abs($amount), "withdraw");
+        }
     }
 }
--- OOP/index.php
+++ OOP/index.php
@@ -10,31 +10,54 @@
     <?php include 'class.php'; ?>
 
     <?php
-    $account = new BankAccount();
-    $customer = new Customer();
-    $customer->setName("John Doe");
-    $customer->setName("");
-    $customer->name = "JOHN NIBATO"; // direct access to public property
+    //create account
+    $account1 = new Account("123");
+    $account2 = new Account("222");
+    $account3 = new Account("222");
 
-    $account->setAccountNumber("1");
+    //create bank account
+    $saveaccount = new SavingsAccount(100000, 3); 
+    $checkaccount = new CheckingAccount(1000,100);
+    $saveaccount1 = new SavingsAccount(1000, 3); 
 
-    $account->transaction(100, 'deposit');
-    $account->transaction(100, 'deposit');
-    $account->transaction(50, 'withdraw');
+    //create customer
+    $customer1 = new Customer("JOHN DOE","JD@gmail.com","123","OC");
+    $customer2 = new Customer("JOHN DOE","JDS@gmail.com","222","OC");
+    $customer1->setName("John nibato");
 
-    // $account->deposit(100);
-    // $account->deposit(50);
-    // $account->withdraw(200);
-    // $account->withdraw(100);
+    $customer1->openAccount($account1,$saveaccount);
+    $customer1->openAccount($account2,$checkaccount);
+    $customer2->openAccount($account3,$saveaccount1);
 
-    // $account->deposit(200)->deposit(50)->deposit(25)->withdraw(75); // method chaining
+    $bank = new Bank(); // create bank
 
-    echo "<h2>Account Details:</h2>";
-    echo "Account Number: " . $account->getAccountNumber() . "<br>";
-    echo "Balance: " . $account->getbalance() . "<br>";
+    $bank->addCustomer($customer1);
+    $bank->addCustomer($customer2);
+    
+    $bank->processTransaction($customer1->getAccounts()[0]['bankaccount'], 1000);
+    $bank->processTransaction($customer1->getAccounts()[1]['bankaccount'], -500);
+    $bank->processTransaction($customer1->getAccounts()[1]['bankaccount'], -100);
+    $bank->processTransaction($customer1->getAccounts()[0]['bankaccount'], -10000);
 
-    echo "<h2>Customer Details:</h2>";
-    echo "Customer Name: " . $customer->getName() . "<br>";
+    $saveaccount->addInterest();
+    // $customer2->closeAccount($account3);
+    //display
+
+
+    foreach($bank->getCustomer() as $customer){
+        echo "<h2>Customer Details:</h2>";
+        echo "Name: " . $customer->getName() . "<br>";
+        echo "Email: " . $customer->getEmail() . "<br>";
+        echo "Phone: " . $customer->getPhone() . "<br>";
+        echo "Address: " . $customer->getAddress() . "<br>";
+        echo "<br>";
+        foreach($customer->getAccounts() as $account){
+            echo "<br>";
+            echo "Account Number: " . $account['account']->getAccountNumber() . "<br>";
+            echo "Balance: " . $account['bankaccount']->getbalance() . "<br>";
+        }
+        echo "<br> <br>";
+    }
 
     
     ?>
